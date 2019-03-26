4d629b8c06e81c7dde9d3d88bbd9097961c75613
Stephanie <Stephaniemkwon@gmail.com>
2019-03-26 13:06:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { compileFunction } from 'vm';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      title: "Shiba Inu",
+      description: "Good boy",
+      imagePath: "./images/img1.jpg"
+    }
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="card">
+        <img className="card-img-top" src={this.state.imagePath} alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">{this.state.title}</h5>
+            <p className="card-text">{this.state.description}</p>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
